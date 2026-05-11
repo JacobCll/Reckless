591adf0221add496b92cde787d518b8340f03f55
JacobCll <jacobcarbonell06@gmail.com>
2026-05-11 12:27:23 +0800

    entity slash visual effect

--- entities/blue_entity_base.gd
+++ entities/blue_entity_base.gd
@@ -1,11 +1,8 @@
+class_name BlueEntityBase
 extends RigidBody2D
 
 signal despawned
 signal slashed
-
-@export var left_half_texture: Texture2D
-@export var right_half_texture: Texture2D
-@export var sprite_half_scene: PackedScene
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -17,24 +14,14 @@ func _on_mouse_entered() -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		slash()
 
+# modify this in individual entities
+func spawn_halves():
+	pass
+
 func slash():
 	slashed.emit()
 	
 	# spawn two halves of the sprite for the slashing visual effect
-	spawn_half(left_half_texture, Vector2(-200, -400))
-	spawn_half(right_half_texture, Vector2(200, -400))
+	spawn_halves()
 
 	queue_free()
-
-# spawn the two halves for the slashing visual effect
-func spawn_half(texture: Texture2D, impulse: Vector2):
-	var half = sprite_half_scene.instantiate()
-
-	get_parent().add_child(half)
-
-	half.global_position = global_position
-	
-	half.velocity = impulse
-	
-	half.get_node("Half").texture = texture
-	half.get_node("Half").scale = Vector2(0.15, 0.15)
--- entities/entity_half.gd
+++ entities/entity_half.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Sprite2D
 
 var velocity := Vector2.ZERO
 var gravity := 1200.0
@@ -7,5 +7,4 @@ var rotation_speed := 5.0
 func _process(delta):
 	velocity.y += gravity * delta
 	position += velocity * delta
-
 	rotation += rotation_speed * delta
