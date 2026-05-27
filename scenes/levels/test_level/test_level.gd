e4d2401553e730626fb1177e588fe258e6f7321d
JacobCll <jacobcarbonell06@gmail.com>
2026-05-27 18:04:31 +0800

    Web export config

--- scenes/levels/test_level/test_level.gd
+++ scenes/levels/test_level/test_level.gd
@@ -171,3 +171,6 @@ func _on_resume_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_back_to_level_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_menu/level_menu.tscn")
