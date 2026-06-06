6017d29fd251916665a96fb6c917372bc3801ab7
JacobCll <jacobcarbonell06@gmail.com>
2026-06-06 23:56:59 +0800

    settings on ready improvement

--- settings/settings_menu.gd
+++ settings/settings_menu.gd
@@ -4,8 +4,17 @@ extends CanvasLayer
 @onready var sfx_slider = $Control/SfxSlider
 
 func _ready():
+	# 1. Temporarily disconnect the signal
+	music_slider.value_changed.disconnect(_on_music_slider_value_changed)
+	sfx_slider.value_changed.disconnect(_on_sfx_slider_value_changed)
+
+	# 2. Update the value (this won't fire the signal anymore)
 	music_slider.value = Settings.music_volume
 	sfx_slider.value = Settings.sfx_volume
+
+	# 3. Reconnect the signal for future user interaction
+	music_slider.value_changed.connect(_on_music_slider_value_changed)
+	sfx_slider.value_changed.connect(_on_sfx_slider_value_changed)
 	
 	_apply_audio()
 	
