fd5c0fed6e674b540a3926316d713712c9b92d80
JacobCll <jacobcarbonell06@gmail.com>
2026-06-09 12:51:24 +0800

    re-export of last commit

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780979780|3823320934';
+const CACHE_VERSION = '1780980630|4673317180';
 /** @type {string} */
 const CACHE_PREFIX = 'Reckless-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
