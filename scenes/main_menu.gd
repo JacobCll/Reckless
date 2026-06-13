78fe58fc06f7e5f3925636a6035f24c8f3f6d0d8
IZYLLE ANNE V. ALBARICO <izylleanne.albarico@gmail.com>
2026-06-13 23:12:25 +0800

    revised ui levels

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	AudioManager.disable_mouse_sfx()
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_menu/level_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_menu/LevelSelect.tscn")
 
 func _on_settings_button_pressed() -> void:
 	MouseManager.hide_mouse_trail()
