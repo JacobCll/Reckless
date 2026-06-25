a5340da1515988d1ea3835aabac57ac79c559eb2
IZYLLE ANNE V. ALBARICO <izylleanne.albarico@gmail.com>
2026-06-25 21:46:02 +0800

    complete ui notification and modify ui of shop

--- notification/notificationscreen.gd
+++ notification/notificationscreen.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_message_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://notification/viewmessage.tscn")
