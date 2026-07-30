b4f27c6a2c84dd68d5c728a6e7051b40ba25ea32
JacobCll <jacobcarbonell06@gmail.com>
2026-07-30 21:56:54 +0800

    Power up modal title fix

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785417290|81608345';
+const CACHE_VERSION = '1785419797|2588946353';
 /** @type {string} */
 const CACHE_PREFIX = 'Reckless-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
