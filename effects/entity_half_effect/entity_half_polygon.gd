7ec48b848f11403828f30fe3b584cf833dd505bb
JacobCll <jacobcarbonell06@gmail.com>
2026-08-06 15:05:49 +0800

    Added blue entities, changed bg for level 2

---
+++ effects/entity_half_effect/entity_half_polygon.gd
@@ -0,0 +1,19 @@
+extends Polygon2D
+
+var velocity := Vector2.ZERO
+var gravity := 1400.0
+var rotation_speed: float
+
+func _ready() -> void:
+	rotation_speed = randf_range(-3.0, 3.0)
+	gravity = randf_range(1200.0, 1600.0)
+
+func _process(_delta):
+	velocity.y += gravity * _delta
+	velocity.x *= 0.995 # optional slight slowdown
+	position += velocity * _delta
+	rotation += rotation_speed * _delta
+
+	# delete when goes out of bounds
+	if global_position.x < -200 or global_position.x > 1224 or global_position.y < -200 or global_position.y > 968:
+		queue_free()
--- effects/entity_half_effect/entity_sprite_halves.gd
+++ effects/entity_half_effect/entity_sprite_halves.gd
@@ -2,18 +2,39 @@
 extends Node2D
 
 @export var entity_half_scene: PackedScene
+@export var entity_half_polygon_scene: PackedScene
 
-# spawn the two halves for the slashing visual effect
+# spawn a whole-texture half for the slashing visual effect
 func spawn_half(texture: Texture2D, impulse: Vector2):
 	var half = entity_half_scene.instantiate()
-	var sprite_2d_pos = get_parent().get_node("Sprite2D").global_position
-	var sprite_2d_rotation = get_parent().get_node("Sprite2D").global_rotation
-	var sprite_2d_scale = get_parent().get_node("Sprite2D").scale
-	
-	get_parent().get_parent().add_child(half)
-	
 	half.texture = texture
-	half.global_position = sprite_2d_pos
-	half.global_rotation = sprite_2d_rotation
-	half.scale = sprite_2d_scale * randf_range(0.7, 1.1)
+	_place_half(half, impulse)
+
+# spawn a half clipped to an arbitrary (non-rectangular) region of a shared
+# texture. `template` is a Polygon2D whose polygon/uv/texture are hand-traced
+# in the editor (Polygon2D UV tool) over the slashed/smashed state image.
+func spawn_half_polygon(template: Polygon2D, impulse: Vector2):
+	var half = entity_half_polygon_scene.instantiate()
+	var tex_size = template.texture.get_size()
+
+	half.texture = template.texture
+	half.uv = template.uv if template.uv.size() > 0 else template.polygon
+	half.polygon = _center_on_texture(template.polygon, tex_size)
+
+	_place_half(half, impulse)
+
+func _center_on_texture(polygon: PackedVector2Array, tex_size: Vector2) -> PackedVector2Array:
+	var centered := PackedVector2Array()
+	for point in polygon:
+		centered.append(point - tex_size / 2.0)
+	return centered
+
+func _place_half(half: Node2D, impulse: Vector2) -> void:
+	var sprite_2d = get_parent().get_node("Sprite2D")
+
+	get_parent().get_parent().add_child(half)
+
+	half.global_position = sprite_2d.global_position
+	half.global_rotation = sprite_2d.global_rotation
+	half.scale = sprite_2d.scale * randf_range(0.7, 1.1)
 	half.velocity = impulse
