d258e00d62f604ac69514d54fd1c682b3422cc8b
JacobCll <jacobcarbonell06@gmail.com>
2026-09-19 15:33:06 +0800

    Web export

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788917125|5088548681';
+const CACHE_VERSION = '1789803118|5602964921';
 /** @type {string} */
 const CACHE_PREFIX = 'Reckless-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
